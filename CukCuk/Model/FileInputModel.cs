bd0f9e15fc71378ef3bd2b8c8a87ab1de769b102
Dinh huy <dinhhuy1798@gmail.com>
2020-08-06 17:31:09 +0700

    upload anh

---
+++ CukCuk/Model/FileInputModel.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CukCuk.Model
+{
+    public class FileInputModel
+    {
+        public Customer Customerr { get; set; }
+        public IFormFile Image { get; set; }
+    }
+}
