299cf0daf9b7a5344aab810211fc42bd6c1fbe9e
Dinh huy <dinhhuy1798@gmail.com>
2020-08-09 10:44:09 +0700

    chinh sua ui dialog

--- CukCuk/wwwroot/js/page/employee.js
+++ CukCuk/wwwroot/js/page/employee.js
@@ -1,1 +1,39 @@
-﻿
+﻿$(document).ready(function () {
+    employeeJS = new EmployeeJS();
+})
+
+/**
+ * Object quản lý các sự kiện cho danh mục nhân viên
+ * CreatedBy: NDHuy (08/08/2020)
+ * */
+class EmployeeJS {
+    constructor() {
+        try {
+            this.loadData();
+            this.initEvent();
+        } catch (e) {
+            console(e);
+        }
+    }
+
+    /**
+     * Thực hiện gán các sự kiện cho các thành phần trong trang
+     * CreatedBy: NDHuy (08/08/2020)
+     * */
+    initEvent() {
+
+    }
+
+    /**
+     * Load dữ liệu vào table Nhân viên
+     * CreatedBy: NDHuy (08/08/2020)
+     * */
+    loadData() {
+        try {
+            $("table#tbListEmployee").empty();
+            //Gọi Ajax lấy dữ liệu về
+        } catch (e) {
+            console(e);
+        }
+    }
+}
